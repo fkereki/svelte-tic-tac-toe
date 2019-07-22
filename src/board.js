fa952d9b0ce54902af829f51b132a56361d7cf19
Federico Kereki <fkereki@gmail.com>
2019-07-22 13:37:11 -0300

    Fixed bug in original getWinner code

--- src/board.js
+++ src/board.js
@@ -29,20 +29,22 @@ export function TicTacToe({ onWin, onTie }) {
 
 function getWinner(board) {
     for (let i = 0; i < 3; i++) {
-        if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-            return board[i][0];
+        if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+            return board[i][1];
         }
-        if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
-            return board[0][i];
+        if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+            return board[1][i];
         }
     }
 
     if (
-        (board[0][0] === board[1][1] && board[1][1] == board[2][2]) ||
-        (board[0][2] === board[1][1] && board[1][1] == board[2][0])
+        (board[0][0] && board[0][0] === board[1][1] && board[1][1] == board[2][2]) ||
+        (board[0][2] && board[0][2] === board[1][1] && board[1][1] == board[2][0])
     ) {
         return board[1][1];
     }
+
+    return null;
 }
 
 function isBoardFull(board) {
