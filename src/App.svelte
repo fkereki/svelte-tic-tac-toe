21b74a8b2e8bd4903a7090234c1a8c073233db57
Federico Kereki <fkereki@gmail.com>
2019-07-24 07:44:29 -0300

    Use derived values in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,34 @@
 <script>
     import Board from "./Board.svelte";
     import { TicTacToe } from "./board";
-    import { message, game, done, turn } from "./store";
+    import { game, turn } from "./store";
+
+    let message = ``;
+    let done = false;
+
+    $: [message, done] = (() => {
+        const winner = $game.getWinner();
+        if (winner) {
+            return [`${winner} wins!`, true];
+        } else if ($game.isBoardFull()) {
+            return [`Tie game!`, true];
+        } else {
+            return [``, false];
+        }
+    })();
 
     createGame();
 
     function createGame() {
-        $message = "";
-        $done = false;
         $turn = "X";
         $game = new TicTacToe();
     }
 
     function onPlayed(event) {
-        $game.click(event.detail.rowIndex, event.detail.colIndex, $turn);
-        $turn = $turn === "X" ? "O" : "X";
-        $game = $game; // needed for reactivity
-
-        const winner = $game.getWinner();
-        if (winner) {
-            $message = `${winner} wins!`;
-            $done = true;
-        } else if ($game.isBoardFull()) {
-            $message = `Tie game!`;
-            $done = true;
+        if (!done) {
+            $game.click(event.detail.rowIndex, event.detail.colIndex, $turn);
+            $turn = $turn === "X" ? "O" : "X";
+            $game = $game; // needed for reactivity
         }
     }
 </script>
@@ -43,7 +48,7 @@
 <button on:click={createGame}>New Game</button>
 
 {#if message}
-    <h2>{$message}</h2>
+    <h2>{message}</h2>
 {/if}
 
 <Board game={$game} on:played={onPlayed} />
--- src/store.js
+++ src/store.js
@@ -1,6 +1,4 @@
 import { writable } from "svelte/store";
 
 export const game = writable([]);
-export const message = writable("");
-export const done = writable(false);
 export const turn = writable("");
