3e868147d66a53e0f47ca5fd858c7847bf3925ae
Federico Kereki <fkereki@gmail.com>
2019-11-23 21:09:42 -0300

    Simplify code

--- src/board.story.js
+++ src/board.story.js
@@ -4,28 +4,24 @@ import Board from "./board.svelte";
 import MyNewBoard from "./board.test2.svelte";
 import { TicTacToe } from "./ticTacToe.class";
 
-const game = new TicTacToe();
-const onPlayed = decorate([x => [x[0].detail]]).action("played!");
-const onPlayed2 = e => window.console.log("clickity!", e.detail);
-
 export default { title: "Board" };
 
 export const BoardWithActions = () => ({
     Component: Board,
     props: {
-        game
+        game: new TicTacToe()
     },
     on: {
-        played: onPlayed
+        played: decorate([x => [x[0].detail]]).action("played!")
     }
 });
 
 export const BoardWithConsole = () => ({
     Component: MyNewBoard,
     props: {
-        game
+        game: new TicTacToe()
     },
     on: {
-        played: onPlayed2
+        played: e => window.console.log("clickity!", e.detail)
     }
 });
